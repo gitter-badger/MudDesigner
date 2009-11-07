272e752bb79dd4e51741a18a7ee9b8bc2ed10118
Scionwest_cp <SND\Scionwest_cp>
2009-11-06 22:41:45 -0800

    MUDEngine:  * Included missing directory       - Objects/Environment  * Included missing files       - Objects/Environment/Door.cs       - Objects/Environment/Realm.cs       - Objects/Environment/Room.cs       - Objects/Environment/Zone.cs

---
+++ MUDEngine/Objects/Environment/Zone.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MUDEngine.Objects.Environment
+{
+    public class Zone : BaseObject
+    {
+    }
+}
