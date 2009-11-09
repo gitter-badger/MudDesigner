7a8437763bb82e88b56b7e67012b0508d25ae7fc
Scionwest_cp <SND\Scionwest_cp>
2009-11-08 21:07:25 -0800

    Room Designer:  * Additional User Interface design changes.
    The Room Designer UI layout has not been decided on as of yet, and thus numerous changes will probably be made to it over the next several days.

--- RoomDesigner/frmMain.Designer.cs
+++ RoomDesigner/frmMain.Designer.cs
@@ -32,13 +32,29 @@
             this.containerSidebar = new System.Windows.Forms.SplitContainer();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
-            this.tabObjects = new System.Windows.Forms.TabControl();
+            this.tabSidebar = new System.Windows.Forms.TabControl();
+            this.tabObjects = new System.Windows.Forms.TabPage();
+            this.tabDoors = new System.Windows.Forms.TabPage();
+            this.containerDesigner = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabBooks = new System.Windows.Forms.TabPage();
+            this.tabEquipment = new System.Windows.Forms.TabPage();
+            this.tabItems = new System.Windows.Forms.TabPage();
             this.containerMain.Panel1.SuspendLayout();
+            this.containerMain.Panel2.SuspendLayout();
             this.containerMain.SuspendLayout();
             this.containerSidebar.Panel1.SuspendLayout();
             this.containerSidebar.Panel2.SuspendLayout();
             this.containerSidebar.SuspendLayout();
             this.groupBox4.SuspendLayout();
+            this.tabSidebar.SuspendLayout();
+            this.tabObjects.SuspendLayout();
+            this.containerDesigner.Panel1.SuspendLayout();
+            this.containerDesigner.Panel2.SuspendLayout();
+            this.containerDesigner.SuspendLayout();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // containerMain
@@ -51,6 +67,10 @@
             // containerMain.Panel1
             // 
             this.containerMain.Panel1.Controls.Add(this.containerSidebar);
+            // 
+            // containerMain.Panel2
+            // 
+            this.containerMain.Panel2.Controls.Add(this.containerDesigner);
             this.containerMain.Size = new System.Drawing.Size(784, 564);
             this.containerMain.SplitterDistance = 225;
             this.containerMain.TabIndex = 0;
@@ -69,7 +89,7 @@
             // 
             // containerSidebar.Panel2
             // 
-            this.containerSidebar.Panel2.Controls.Add(this.tabObjects);
+            this.containerSidebar.Panel2.Controls.Add(this.tabSidebar);
             this.containerSidebar.Size = new System.Drawing.Size(225, 564);
             this.containerSidebar.SplitterDistance = 294;
             this.containerSidebar.TabIndex = 0;
@@ -93,14 +113,114 @@
             this.propertyGrid1.Size = new System.Drawing.Size(217, 273);
             this.propertyGrid1.TabIndex = 0;
             // 
+            // tabSidebar
+            // 
+            this.tabSidebar.Controls.Add(this.tabObjects);
+            this.tabSidebar.Controls.Add(this.tabDoors);
+            this.tabSidebar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabSidebar.Location = new System.Drawing.Point(0, 0);
+            this.tabSidebar.Name = "tabSidebar";
+            this.tabSidebar.SelectedIndex = 0;
+            this.tabSidebar.Size = new System.Drawing.Size(223, 264);
+            this.tabSidebar.TabIndex = 0;
+            // 
             // tabObjects
             // 
-            this.tabObjects.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabObjects.Location = new System.Drawing.Point(0, 0);
+            this.tabObjects.Controls.Add(this.tabControl1);
+            this.tabObjects.Location = new System.Drawing.Point(4, 22);
             this.tabObjects.Name = "tabObjects";
-            this.tabObjects.SelectedIndex = 0;
-            this.tabObjects.Size = new System.Drawing.Size(223, 264);
+            this.tabObjects.Padding = new System.Windows.Forms.Padding(3);
+            this.tabObjects.Size = new System.Drawing.Size(215, 238);
             this.tabObjects.TabIndex = 0;
+            this.tabObjects.Text = "Objects";
+            this.tabObjects.UseVisualStyleBackColor = true;
+            // 
+            // tabDoors
+            // 
+            this.tabDoors.Location = new System.Drawing.Point(4, 22);
+            this.tabDoors.Name = "tabDoors";
+            this.tabDoors.Padding = new System.Windows.Forms.Padding(3);
+            this.tabDoors.Size = new System.Drawing.Size(215, 238);
+            this.tabDoors.TabIndex = 1;
+            this.tabDoors.Text = "Doors";
+            this.tabDoors.UseVisualStyleBackColor = true;
+            // 
+            // containerDesigner
+            // 
+            this.containerDesigner.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.containerDesigner.Location = new System.Drawing.Point(0, 0);
+            this.containerDesigner.Name = "containerDesigner";
+            this.containerDesigner.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // containerDesigner.Panel1
+            // 
+            this.containerDesigner.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // containerDesigner.Panel2
+            // 
+            this.containerDesigner.Panel2.Controls.Add(this.groupBox2);
+            this.containerDesigner.Size = new System.Drawing.Size(553, 562);
+            this.containerDesigner.SplitterDistance = 209;
+            this.containerDesigner.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(553, 209);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Room Search Options";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(553, 349);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Room Design Preview";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabBooks);
+            this.tabControl1.Controls.Add(this.tabEquipment);
+            this.tabControl1.Controls.Add(this.tabItems);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(3, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(209, 232);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabBooks
+            // 
+            this.tabBooks.Location = new System.Drawing.Point(4, 22);
+            this.tabBooks.Name = "tabBooks";
+            this.tabBooks.Size = new System.Drawing.Size(201, 206);
+            this.tabBooks.TabIndex = 0;
+            this.tabBooks.Text = "Books";
+            this.tabBooks.UseVisualStyleBackColor = true;
+            // 
+            // tabEquipment
+            // 
+            this.tabEquipment.Location = new System.Drawing.Point(4, 22);
+            this.tabEquipment.Name = "tabEquipment";
+            this.tabEquipment.Size = new System.Drawing.Size(201, 206);
+            this.tabEquipment.TabIndex = 1;
+            this.tabEquipment.Text = "Equipment";
+            this.tabEquipment.UseVisualStyleBackColor = true;
+            // 
+            // tabItems
+            // 
+            this.tabItems.Location = new System.Drawing.Point(4, 22);
+            this.tabItems.Name = "tabItems";
+            this.tabItems.Size = new System.Drawing.Size(201, 206);
+            this.tabItems.TabIndex = 2;
+            this.tabItems.Text = "Items";
+            this.tabItems.UseVisualStyleBackColor = true;
             // 
             // frmMain
             // 
@@ -115,11 +235,18 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Mud Designer: Room Designer";
             this.containerMain.Panel1.ResumeLayout(false);
+            this.containerMain.Panel2.ResumeLayout(false);
             this.containerMain.ResumeLayout(false);
             this.containerSidebar.Panel1.ResumeLayout(false);
             this.containerSidebar.Panel2.ResumeLayout(false);
             this.containerSidebar.ResumeLayout(false);
             this.groupBox4.ResumeLayout(false);
+            this.tabSidebar.ResumeLayout(false);
+            this.tabObjects.ResumeLayout(false);
+            this.containerDesigner.Panel1.ResumeLayout(false);
+            this.containerDesigner.Panel2.ResumeLayout(false);
+            this.containerDesigner.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -128,9 +255,18 @@
 
         private System.Windows.Forms.SplitContainer containerMain;
         private System.Windows.Forms.SplitContainer containerSidebar;
-        private System.Windows.Forms.TabControl tabObjects;
+        private System.Windows.Forms.TabControl tabSidebar;
         private System.Windows.Forms.GroupBox groupBox4;
         private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.TabPage tabObjects;
+        private System.Windows.Forms.TabPage tabDoors;
+        private System.Windows.Forms.SplitContainer containerDesigner;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabBooks;
+        private System.Windows.Forms.TabPage tabEquipment;
+        private System.Windows.Forms.TabPage tabItems;
     }
 }
 
