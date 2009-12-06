ea9a67ea48350b28b6b37ea5b2bb3dda4e6a9d67
Scionwest_cp <SND\Scionwest_cp>
2009-12-06 00:14:06 -0800

    Mud Engine:  - Added missing TravelDirections file.

---
+++ Mud Designer/MudEngine/GameObjects/TravelDirections.cs
@@ -0,0 +1,39 @@
+﻿namespace MudDesigner.MudEngine.Objects
+{
+    public enum AvailableTravelDirections
+    {
+        None = 0,
+        North,
+        South,
+        East,
+        West,
+        Up,
+        Down,
+    }
+
+    public static class TravelDirections
+    {
+        public static AvailableTravelDirections GetReverseDirection(AvailableTravelDirections Direction)
+        {
+            switch (Direction)
+            {
+                case AvailableTravelDirections.Down:
+                    return AvailableTravelDirections.Up;
+                case AvailableTravelDirections.East:
+                    return AvailableTravelDirections.West;
+                case AvailableTravelDirections.None:
+                    return AvailableTravelDirections.None;
+                case AvailableTravelDirections.North:
+                    return AvailableTravelDirections.South;
+                case AvailableTravelDirections.South:
+                    return AvailableTravelDirections.North;
+                case AvailableTravelDirections.Up:
+                    return AvailableTravelDirections.Down;
+                case AvailableTravelDirections.West:
+                    return AvailableTravelDirections.East;
+                default:
+                    return AvailableTravelDirections.None;
+            }
+        }
+    }
+}
