85aae88e34e21c3a0ca326f2cb54680bdf3182c9
Scionwest_cp <SND\Scionwest_cp>
2010-01-22 22:14:08 -0800

    Engine:  - Added GameCommands namespace for holding all of the game command classes  - Added ICommand interface for game commands.  - Added IPlayer interface for player classes.  - Added CommandLook for prepping the Test Runtime for looking at environments.
    Misc:
     - Updated Mud Designer Project Roadmap file (MudDesigner.pod)

---
+++ Mud Designer/MudEngine/GameCommands/CommandLook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MudDesigner.MudEngine.FileSystem;
+using MudDesigner.MudEngine.GameCommands;
+using MudDesigner.MudEngine.GameManagement;
+using MudDesigner.MudEngine.GameObjects.Environment;
+using MudDesigner.MudEngine.GameObjects.Items;
+using MudDesigner.MudEngine.Interfaces;
+
+namespace MudDesigner.MudEngine.GameCommands
+{
+    public class CommandLook : IGameCommand
+    {
+        public string Name { get; set; }
+        public bool Override { get; set; }
+
+        public object Execute(params object[] parameters)
+        {
+            Room room = new Room() ;
+            StringBuilder desc = new StringBuilder();
+
+            foreach (object obj in parameters)
+            {
+                if (obj is Room)
+                    room = (Room)obj;
+            }
+
+            if (room == null)
+                return null;
+
+            foreach (Door door in room.Doorways)
+            {
+                if (door.TravelDirection != MudDesigner.MudEngine.GameObjects.AvailableTravelDirections.Down && door.TravelDirection != MudDesigner.MudEngine.GameObjects.AvailableTravelDirections.Up)
+                {
+                    desc.AppendLine("To the ");
+                    desc.Append(door.TravelDirection.ToString());
+                    desc.Append(" is ");
+                    desc.Append(door.ConnectedRoom);
+                }
+            }
+
+            return desc.ToString();
+        }
+    }
+}
--- Mud Designer/Runtime.cs
+++ Mud Designer/Runtime.cs
@@ -1,21 +1,30 @@
-﻿using System;
+﻿//.Net Framework
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+
+//Mud Designer
+using MudDesigner.MudEngine.GameCommands;
+using MudDesigner.MudEngine.FileSystem;
+using MudDesigner.MudEngine.GameManagement;
+using MudDesigner.MudEngine.GameObjects;
+using MudDesigner.MudEngine.GameObjects.Environment;
+using MudDesigner.MudEngine.GameObjects.Items;
 
 namespace MudDesigner
 {
     public partial class Runtime : Form
     {
+        
         public Runtime()
         {
             InitializeComponent();
-
-            
         }
 
         private void txtCommand_TextChanged(object sender, EventArgs e)
