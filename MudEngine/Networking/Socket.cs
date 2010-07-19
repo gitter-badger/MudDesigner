8381349143e04a66d8b9d419b6b6ce248ef178a8
u8sand_cp <SND\u8sand_cp>
2010-07-18 20:31:30 -0700

    Wups, forgot to "add" this.

---
+++ MudEngine/Networking/Socket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// TODO: everything D:
+
+namespace MudEngine.Networking
+{
+    class Socket
+    {
+        public Socket()
+        {
+        }
+        public ~Socket()
+        {
+        }
+
+    }
+}
