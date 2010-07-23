f7756e20328ab9343257f1ea03f9950def42d263
Scionwest_cp <SND\Scionwest_cp>
2010-07-22 18:01:40 -0700

    MudEngine:  - Added missing PlayerAdmin class.

---
+++ MudEngine/GameObjects/Characters/Controlled/PlayerAdmin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MudEngine.GameObjects.Characters.Controlled
+{
+    public class PlayerAdmin : PlayerBasic
+    {
+    }
+}
