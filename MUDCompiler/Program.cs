7bc89b294b90bab16dcab863d0f730fcf9ba8db2
Scionwest_cp <SND\Scionwest_cp>
2011-09-18 14:04:19 -0700

    Re-wrote the MudCompiler project.  It now supports compiling whole directories or single files using the new scripting engine.

--- MUDCompiler/Program.cs
+++ MUDCompiler/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 using MudEngine.Scripting;
 
@@ -9,44 +10,10 @@ namespace MUDCompiler
 {
     class Program
     {
+        [STAThread()]
         static void Main(String[] args)
         {
-            Console.WriteLine("===========================");
-            Console.WriteLine("MUD Engine Content Compiler");
-            Console.WriteLine("Version 0.1");
-            Console.WriteLine("===========================");
-            Console.WriteLine();
-            Console.WriteLine("1): Compile Scripts");
-            Console.WriteLine("2): Exit Compiler");
-            //Console.Write("Enter Selection: ");
-            Console.WriteLine("Out of engine compiling is currently not supported.  Please review the documentation regarding in-engine compiling.");
-
-            String command = Console.ReadLine();
-
-            //command error checking.
-            if (String.IsNullOrEmpty(command))
-            {
-                Console.WriteLine("Invalid Command!");
-                System.Threading.Thread.Sleep(1000); //wait before shutting down so user sees invalid command
-            }
-            else if (Convert.ToInt16(command) >= 3)
-            {
-                Console.WriteLine("Invalid Command!");
-                System.Threading.Thread.Sleep(1000); //wait before shutting down so user sees invalid command
-            }
-
-            switch (Convert.ToInt16(command))
-            {
-                case 1:
-                    CompileScripts();
-                    break;
-                case 2:
-                    return;
-            }
-        }
-
-        static void CompileScripts()
-        {
+            Application.Run(new frmCompiler());
         }
     }
 }
---
+++ MUDCompiler/frmCompiler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MUDCompiler
+{
+    public partial class frmCompiler : Form
+    {
+        public frmCompiler()
+        {
+            InitializeComponent();
+        }
+
+        private void openDirectoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rScripting.CompileEngine engine = new rScripting.CompileEngine();
+
+            FolderBrowserDialog browse = new FolderBrowserDialog();
+            browse.ShowDialog();
+
+            if (String.IsNullOrEmpty(browse.SelectedPath))
+            {
+                txtConsole.Text = "Compilation canceled.";
+                return;
+            }
+
+            bool isOK = engine.Compile(browse.SelectedPath);
+
+            if (isOK)
+                txtConsole.Text = "Compiled without error.";
+            else
+                txtConsole.Text = engine.Errors;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void openScriptToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rScripting.CompileEngine engine = new rScripting.CompileEngine();
+
+            OpenFileDialog browse = new OpenFileDialog();
+            browse.ShowDialog();
+
+            System.IO.FileInfo file = new System.IO.FileInfo(browse.FileName);
+
+            if (String.IsNullOrEmpty(browse.FileName))
+            {
+                txtConsole.Text = "Compilation canceled.";
+                return;
+            }
+
+            bool isOK = engine.Compile(file);
+            if (isOK)
+                txtConsole.Text = "Compiled without error.";
+            else
+                txtConsole.Text = engine.Errors;
+        }
+    }
+}
