5be2f9bf5b72e71b9777eed74d01f74de726cf66
Scionwest_cp <SND\Scionwest_cp>
2011-10-01 22:09:33 -0700

    Removed old engine classes.  Needs to be done prior to checking-in the new engine classes re-wrote from ground up.

--- Engine/Core/BaseGame.cs
+++ Engine/Core/BaseGame.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace MudEngine.Core
+namespace Engine.Core
 {
     public abstract class BaseGame
     {
+
     }
 }
---
+++ Engine/Core/IObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine.Core
+{
+    public class IObject
+    {
+        public string Name { get; set; }
+
+    }
+}
