e6ca7b28636e90ab717c564e27a6e6a6e68560a5
Scionwest_cp <SND\Scionwest_cp>
2012-10-24 20:12:52 -0700

    Engine: - Updated diagram.

--- MudDesigner/Engine/Objects/IEquipment.cs
+++ MudDesigner/Engine/Objects/IEquipment.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using MudDesigner.Engine.Actions;
+
 namespace MudDesigner.Engine.Objects
 {
     public interface IEquipment : IInventory
