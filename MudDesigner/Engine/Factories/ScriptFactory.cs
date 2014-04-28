135517ca65e22d498b150d442ba7f4874a4b52c2
Scionwest_cp <SND\Scionwest_cp>
2014-04-27 17:19:08 -0700

    Moved ScriptFactory into the Factories namespace.

--- MudDesigner/Engine/Factories/ScriptFactory.cs
+++ MudDesigner/Engine/Factories/ScriptFactory.cs
@@ -9,7 +9,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 
-namespace MudEngine.Engine.Scripting
+namespace MudEngine.Engine.Factories
 {
     /// <summary>
     /// The ScriptFactory provides helper methods for instancing scripted game objects
