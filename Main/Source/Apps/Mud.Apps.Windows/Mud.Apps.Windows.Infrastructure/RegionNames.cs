9826a919543e6c1ff405f61adf520d78068428a6
Scionwest_cp <SND\Scionwest_cp>
2014-08-03 13:05:28 -0700

    Addition of Desktop Infrastructure project.

---
+++ Main/Source/Apps/Mud.Apps.Windows/Mud.Apps.Windows.Infrastructure/RegionNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mud.Apps.Windows.Infrastructure
+{
+    public class RegionNames
+    {
+        public static string ToolbarRegion = "ToolbarRegion";
+        public static string ContentRegion = "ContentRegion";
+    }
+}
