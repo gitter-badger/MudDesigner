a149dcab57d2dc4affc2d97b43abc1d5a1bd20a4
Scionwest_cp <SND\Scionwest_cp>
2014-08-03 12:16:17 -0700

    Initial check-in of new projects for the Mud Designer re-design.

---
+++ Main/Source/Apps/Mud.Apps.Engine/Core/IGame.cs
@@ -0,0 +1,67 @@
+﻿using Mud.Apps.Engine.Environment;
+using Mud.Apps.Engine.Mob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mud.Apps.Engine.Core
+{
+    public interface IGame
+    {
+        /// <summary>
+        /// Gets a value indicating whether this instance is multiplayer.
+        /// </summary>
+        bool IsMultiplayer { get; }
+
+        /// <summary>
+        /// Gets if the game (online or offline) is currently running.
+        /// </summary>
+        bool IsRunning { get; }
+
+        /// <summary>
+        /// Gets or Sets the name of the game being played.
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description of the game.
+        /// </summary>
+        string Description { get; set; }
+
+        /// <summary>
+        /// Gets or Sets the current version of the game.
+        /// </summary>
+        Version Version { get; set; }
+
+        /// <summary>
+        /// Gets or Sets the website that users can visit to get information on the game.
+        /// </summary>
+        string Website { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether [hide room names].
+        /// </summary>
+        bool HideRoomNames { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether [enable automatic save].
+        /// </summary>
+        bool EnableAutoSave { get; set; }
+
+        /// <summary>
+        /// Gets or sets the automatic save frequency.
+        /// </summary>
+        int AutoSaveFrequency { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last saved.
+        /// </summary>
+        DateTime LastSaved { get; set; }
+
+        /// <summary>
+        /// Gets or Sets the current World for the game. Contains all of the Realms, Zones and Rooms.
+        /// </summary>
+        List<IWorld> Worlds { get; set; }
+    }
+}
---
+++ Main/Source/Repositories/Mud.Repositories.Shared/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mud.Repositories.Shared
+{
+    public interface IRepository
+    {
+    }
+}
