39e825e690108b94cafcfe09478bd97d2f9befbc
Scionwest_cp <SND\Scionwest_cp>
2014-08-03 12:56:52 -0700

    Cleaned up project directory. Added initial Prism Bootstrapper for Apps.Windows.Desktop project.

---
+++ Main/Source/Apps/Mud.Apps.Windows/Mud.Apps.Windows.Destop/Bootstrapper.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Practices.Prism.UnityExtensions;
+using Mud.Apps.WinDesktop.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using System.Windows;
+
+namespace Mud.Apps.WinDesktop
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        /// <summary>
+        /// Creates the shell or main window of the application.
+        /// </summary>
+        /// <returns>
+        /// The shell of the application.
+        /// </returns>
+        /// <remarks>
+        /// If the returned instance is a <see cref="T:System.Windows.DependencyObject" />, the
+        /// <see cref="T:Microsoft.Practices.Prism.Bootstrapper" /> will attach the default <see cref="T:Microsoft.Practices.Prism.Regions.IRegionManager" /> of
+        /// the application in its <see cref="F:Microsoft.Practices.Prism.Regions.RegionManager.RegionManagerProperty" /> attached property
+        /// in order to be able to add regions by using the <see cref="F:Microsoft.Practices.Prism.Regions.RegionManager.RegionNameProperty" />
+        /// attached property from XAML.
+        /// </remarks>
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        /// <summary>
+        /// Initializes the shell.
+        /// </summary>
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            App.Current.MainWindow = (Window)Shell;
+            App.Current.MainWindow.Show();
+        }
+    }
+}
