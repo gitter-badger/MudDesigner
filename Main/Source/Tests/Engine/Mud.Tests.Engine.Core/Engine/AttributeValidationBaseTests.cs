c6578dbc5e1028a52d93422cabaf20d80f6a20e2
Scionwest_cp <SND\Scionwest_cp>
2014-08-10 21:25:17 -0700

    Added Engine.Core unit test project

---
+++ Main/Source/Tests/Engine/Mud.Tests.Engine.Core/Engine/AttributeValidationBaseTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mud.Tests.Engine.Core.Engine
+{
+    [TestClass]
+    public class AttributeValidationBaseTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
