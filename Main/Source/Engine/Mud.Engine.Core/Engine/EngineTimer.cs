181842fd9e3f679a804f86e33e8131fc8aed10ae
Scionwest_cp <SND\Scionwest_cp>
2014-08-24 20:43:56 -0700

    EngineTimer can now be used as a one shot timer.

--- Main/Source/Engine/Mud.Engine.Core/Engine/EngineTimer.cs
+++ Main/Source/Engine/Mud.Engine.Core/Engine/EngineTimer.cs
@@ -8,18 +8,39 @@ using System.Threading.Tasks;
 
 namespace Mud.Engine.Core.Engine
 {
+    /// <summary>
+    /// The Engine  Timer provides a means to start a timer with a callback that can be repeated over a set interval.
+    /// </summary>
+    /// <typeparam name="T">A generic Type</typeparam>
     public sealed class EngineTimer<T> : CancellationTokenSource, IDisposable
     {
+        /// <summary>
+        /// The timer task
+        /// </summary>
         private Task timerTask;
 
+        /// <summary>
+        /// The callback
+        /// </summary>
         private Action<T, EngineTimer<T>> callback;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EngineTimer{T}"/> class.
+        /// </summary>
+        /// <param name="callback">The callback.</param>
+        /// <param name="state">The state.</param>
         public EngineTimer(Action<T, EngineTimer<T>> callback, T state)
         {
             this.callback = callback;
             this.StateData = state;
         }
 
+        /// <summary>
+        /// Gets the state data.
+        /// </summary>
+        /// <value>
+        /// The state data.
+        /// </value>
         public T StateData { get; private set; }
 
         /// <summary>
@@ -27,8 +48,9 @@ namespace Mud.Engine.Core.Engine
         /// </summary>
         /// <param name="startDelay">The start delay in milliseconds.</param>
         /// <param name="interval">The interval in milliseconds.</param>
+        /// <param name="isOneShot">if set to <c>true</c> [is one shot].</param>
         /// <param name="callbackOnWorkerThread">if set to <c>true</c> the callback will be performed on a background thread.</param>
-        public void Start(double startDelay, double interval, bool callbackOnWorkerThread = false)
+        public void Start(double startDelay, double interval, bool isOneShot = false, bool callbackOnWorkerThread = false)
         {
             this.timerTask = Task.Delay(TimeSpan.FromMilliseconds(startDelay), Token).ContinueWith(async (task, state) =>
             {
@@ -55,6 +77,12 @@ namespace Mud.Engine.Core.Engine
                     {
                         tuple.Item1(tuple.Item2, this);
                     }
+
+                    if (isOneShot)
+                    {
+                        this.Cancel();
+                    }
+
                     await Task.Delay(TimeSpan.FromMilliseconds(interval), Token).ConfigureAwait(false);
                 }
             }, 
@@ -64,6 +92,9 @@ namespace Mud.Engine.Core.Engine
             TaskScheduler.Default);
         }
 
+        /// <summary>
+        /// Stops the timer for this instance.
+        /// </summary>
         public void Stop()
         {
             if (!this.IsCancellationRequested)
@@ -72,6 +103,10 @@ namespace Mud.Engine.Core.Engine
             }
         }
 
+        /// <summary>
+        /// Releases the unmanaged resources used by the <see cref="T:System.Threading.CancellationTokenSource" /> class and optionally releases the managed resources.
+        /// </summary>
+        /// <param name="disposing">true to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing)
