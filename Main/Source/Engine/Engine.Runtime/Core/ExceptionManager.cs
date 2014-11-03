d9f512c367fbcfb7ca357197eb0efdfa1e78ce09
Johnathon <Johnathon@Sullinger.us>
2014-11-02 20:57:54 -0700

    Added ExceptionManager.ThrowExceptionIf overload taking a Func<bool> predicate

--- Main/Source/Engine/Engine.Runtime/Core/ExceptionManager.cs
+++ Main/Source/Engine/Engine.Runtime/Core/ExceptionManager.cs
@@ -6,13 +6,39 @@ using System.Threading.Tasks;
 
 namespace Mud.Engine.Runtime.Core
 {
+    /// <summary>
+    /// Provides helper methods for quickly throwing exceptions based on conditions.
+    /// </summary>
     public static class ExceptionManager
     {
-        public static void ThrowExceptionIf<T>(bool condition, params KeyValuePair<string, string>[] data) where T : Exception, new()
+        /// <summary>
+        /// Throws the exception if the given condition is true.
+        /// </summary>
+        /// <typeparam name="TException">The type of the exception.</typeparam>
+        /// <param name="condition">if set to <c>true</c> [condition].</param>
+        /// <param name="data">The data assigned to the Exception.Data property.</param>
+        public static void ThrowExceptionIf<TException>(bool condition, params KeyValuePair<string, string>[] data) where TException : Exception, new()
         {
             if (condition)
             {
-                var exception = new T();
+                var exception = new TException();
+                data.AsParallel().ForAll(d => exception.Data.Add(d.Key, d.Value));
+
+                throw exception;
+            }
+        }
+
+        /// <summary>
+        /// Throws the exception if the predicate given evaluates to true.
+        /// </summary>
+        /// <typeparam name="TException">The type of the exception.</typeparam>
+        /// <param name="predicate">The predicate.</param>
+        /// <param name="data">The data assigned to the Exception.Data property.</param>
+        public static void ThrowExceptionIf<TException>(Func<bool> predicate, params KeyValuePair<string, string>[] data) where TException : Exception, new()
+        {
+            if (predicate())
+            {
+                var exception = new TException();
                 data.AsParallel().ForAll(d => exception.Data.Add(d.Key, d.Value));
 
                 throw exception;
